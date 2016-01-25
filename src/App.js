ad971ccac30c4e906af5931f41f9266275e9f9d5
Nick Dainty <nickdainty@gmail.com>
2016-01-25 18:47:58 +1100

    Create a UI wireframe

--- src/App.js
+++ src/App.js
@@ -1,13 +1,55 @@
 import React from 'react';
 import times from 'lodash/utility/times';
-import Frame from './frame';
 
 export default class App extends React.Component {
-  render() {
-    return (<div className="app">
-      <div className="frames">
-        {times(9, () => <Frame/>)}
+  frame() {
+    return (
+      <div className="frame">
+        <div className="throws">
+          <div className="throw first"></div>
+          <div className="throw second"></div>
+        </div>
+        <div className="total"></div>
       </div>
-    </div>);
+    );
+  }
+
+  finalFrame() {
+    return (
+      <div className="frame final-frame">
+        <div className="throws">
+          <div className="throw first"></div>
+          <div className="throw second"></div>
+          <div className="throw third"></div>
+        </div>
+        <div className="total"></div>
+      </div>
+    );
+  }
+
+  pinOption(number) {
+    return (
+      <a className="pins-option">{number}</a>
+    );
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <header>
+          <h1>Bowling</h1>
+          <a className="new-game">Start New Game</a>
+        </header>
+        <h2>Scoreboard</h2>
+        <div className="frames">
+          {times(10, () => this.frame())}
+          {this.finalFrame()}
+        </div>
+        <h2>Number of pins</h2>
+        <div className="scoring">
+          {times(10, (i) => this.pinOption(i + 1))}
+        </div>
+      </div>
+    );
   }
 }
